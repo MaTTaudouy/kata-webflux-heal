0342980551d12469a4a8b54370e07903926e28a3
maudouy <matthieu.audouy@capgemini.com>
2020-04-24 10:13:45 +0200

    Remove unused projects

--- webflux-datatable/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PatientList from './patients';
-
-ReactDOM.render(<PatientList></PatientList>, document.getElementById('root'));
