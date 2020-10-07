6650e34aa70cbdef391d068ac3081a7d12319255
maudouy <matthieu.audouy@capgemini.com>
2020-10-07 10:56:03 +0200

    Update to 2.3.4.Release

---
+++ kataflux-patient-service-eureka/src/test/java/demo/webflux/kata/sample/PatientAPIFunctionalSample.java
@@ -0,0 +1,32 @@
+import demo.webflux.kata.patient.model.Patient;
+import demo.webflux.kata.patient.repository.PatientRepository;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.BodyExtractors.toMono;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Configuration
+public class PatientAPI {
+
+	@Bean
+	public RouterFunction<ServerResponse> routes(PatientRepository patientRepository) {
+		return route()
+				.GET("/patients", serverRequest -> ok()
+						.body(patientRepository.findAll(), Patient.class))
+				.GET("/patients/{id}",
+						serverRequest -> ok().body(
+								patientRepository
+									.findById(Long.valueOf(serverRequest.pathVariable("id"))),
+								Patient.class))
+				.POST("/patients", request -> request.body(toMono(Patient.class))
+						.doOnNext(patientRepository::save)
+						.then(ok().build()))
+				.build();
+	}
+
+}
